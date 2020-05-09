5449414ccbce73925bc56163fcc4267139a6a5c8
DoHyeon Lee <cailhuiris@gmail.com>
2020-05-10 01:16:28 +0900

    Style Transfer 구현

--- pytorch/vision/neural-style-transfer/style-transfer.py
+++ pytorch/vision/neural-style-transfer/style-transfer.py
@@ -20,7 +20,7 @@ STYLE_DATA_PATH     = "/Users/idohyeon/ML-dataset/neural-transfer/style.jpg"
 
 # set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-imsize = 512 if torch.cuda.is_available() else 128
+imsize = 512 if torch.cuda.is_available() else 256
 
 # transforms the image with (resize -> tensor)
 loader = transforms.Compose([
@@ -52,17 +52,10 @@ def imshow(_image, title=None):
         plt.title(title)
     # plt.pause(0.001)
 
-plt.figure()
-imshow(style_image, title="Style Image")
-
-plt.figure()
-imshow(content_image, title="Content Image")
-plt.show()
-
 # define content loss, X -> X'
 class ContentLoss(nn.Module):
-    def __init__(super, target):
-        super(ContentLoss).__init__()
+    def __init__(self, target):
+        super(ContentLoss, self).__init__()
         self.target = target.detach()
 
     def forward(self, input):
@@ -78,6 +71,17 @@ def gram_matrix(input):
     # normalize ?
     return G.div(a*b*c*d)
 
+class StyleLoss(nn.Module):
+    def __init__(self, target_feature):
+        super(StyleLoss, self).__init__()
+        self.target = gram_matrix(target_feature).detach()
+    
+    def forward(self, input):
+        G = gram_matrix(input)
+        self.loss = F.mse_loss(G, self.target)
+        return input
+
+
 # import pre-trained network
 # -> VGG19 모델이 Normalization할 때 이용한 값으로 기존 이미지를 Normalize한다.
 cnn = models.vgg19(pretrained=True).features.to(device).eval()
@@ -85,3 +89,132 @@ cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
 cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
 
 # Normalize
+class Normalize(nn.Module):
+    def __init__(self, mean, std):
+        super(Normalize, self).__init__()
+        self.mean = torch.tensor(mean).view(-1, 1, 1)
+        self.std = torch.tensor(std).view(-1, 1, 1)
+
+    def forward(self, img):
+        # normalize
+        return (img - self.mean) / self.std
+
+# style modeling and losses
+content_layers_default = ['conv_4']
+style_layers_default = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
+
+def get_style_model_and_losses(cnn, normalization_mean, normalization_std,
+                               style_img, content_img,
+                               content_layers=content_layers_default,
+                               style_layers=style_layers_default):
+    cnn = copy.deepcopy(cnn)
+
+    # normalize
+    normalization = Normalize(normalization_mean, normalization_std).to(device)
+    content_losses = []
+    style_losses = []
+    model = nn.Sequential(normalization)
+
+    # vgg19 모델의 각 레이어를 현재 모델에 쌓는다.
+    i = 0
+    for layer in cnn.children():
+        if isinstance(layer, nn.Conv2d):
+            i += 1
+            name = 'conv_{}'.format(i)
+        elif isinstance(layer, nn.ReLU):
+            name = 'relu_{}'.format(i)
+            layer = nn.ReLU(inplace=False)
+        elif isinstance(layer, nn.MaxPool2d):
+            name = 'pool_{}'.format(i)
+        elif isinstance(layer, nn.BatchNorm2d):
+            name = 'bn_{}'.format(i)
+        else:
+            raise RuntimeError('Unrecognized layer: {}'.format(layer.__class__.__name__))
+
+        model.add_module(name, layer)
+
+        # conv4의 결과 loss
+        if name in content_layers:
+            target = model(content_img).detach()
+            content_loss = ContentLoss(target)
+            model.add_module("content_loss_{}".format(i), content_loss)
+            content_losses.append(content_loss)
+
+        # 각 히든 계층의 feature를 학습하는 레이어
+        if name in style_layers:
+            target_feature = model(style_img).detach()
+            style_loss = StyleLoss(target_feature)
+            model.add_module("style_loss_{}".format(i), style_loss)
+            style_losses.append(style_loss)
+
+    for i in range(len(model) - 1, -1, -1):
+        if isinstance(model[i], ContentLoss) or isinstance(model[i], StyleLoss):
+            break
+
+    model = model[:(i + 1)]
+
+    return model, style_losses, content_losses
+
+def get_input_optimizer(input_image):
+    optimizer = optim.LBFGS([input_image.requires_grad_()])
+    return optimizer
+
+def run_style_transfer(cnn, normalization_mean, normalization_std,
+                       content_img, style_image, input_image, num_steps=300,
+                       style_weight=1000000, content_weight=1):
+    print('Building the style transfer model..')
+    model, style_losses, content_losses = get_style_model_and_losses(cnn,
+        normalization_mean, normalization_std, style_image, content_img)
+    optimizer = get_input_optimizer(input_image)
+
+    print('Running..')
+    run = [0]
+    while run[0] <= num_steps:
+        def closure():
+            # correct the values of updated input image
+            input_image.data.clamp_(0, 1)
+
+            optimizer.zero_grad()
+            model(input_image)
+            style_score = 0
+            content_score = 0
+
+            for sl in style_losses:
+                style_score += sl.loss
+            for cl in content_losses:
+                content_score += cl.loss
+
+            style_score *= style_weight
+            content_score *= content_weight
+
+            loss = style_score + content_score
+            loss.backward()
+
+            run[0] += 1
+            if run[0] % 50 == 0:
+                print("run {}:".format(run))
+                print('Style Loss : {:4f} Content Loss: {:4f}'.format(
+                    style_score.item(), content_score.item()))
+                print()
+
+            return style_score + content_score
+
+        optimizer.step(closure)
+
+    input_image.data.clamp_(0, 1)
+    return input_image
+
+input_image = content_image.clone()
+output = run_style_transfer(
+    cnn, 
+    cnn_normalization_mean, 
+    cnn_normalization_std,
+    content_image, 
+    style_image, 
+    input_image,
+    num_steps=1000
+)
+
+plt.figure()
+imshow(output, title='Output Image')
+plt.show()
