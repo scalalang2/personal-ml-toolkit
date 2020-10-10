b0fbe4cf23dbf4d06e86b07fed8d212f47a8ba97
DoHyeon Lee <cailhuiris@gmail.com>
2020-10-10 21:01:04 +0900

    Added tutorial classifier

---
+++ pytorch/tutorial/01-classifier.py
@@ -0,0 +1,130 @@
+import torch
+import torchvision 
+import torchvision.transforms as transforms
+
+# Set DataLoader and Dataset
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+trainset = torchvision.datasets.CIFAR10(
+    root='../../_data',
+    train=True,
+    download=True,
+    transform=transform
+)
+
+trainloader = torch.utils.data.DataLoader(
+    trainset,
+    batch_size=32,
+    shuffle=True,
+    num_workers=2
+)
+
+testset = torchvision.datasets.CIFAR10(
+    root='../../_data',
+    train=False,
+    download=True,
+    transform=transform
+)
+
+testloader = torch.utils.data.DataLoader(
+    testset,
+    batch_size=32,
+    shuffle=True,
+    num_workers=2
+)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+# import
+import matplotlib.pyplot as plt
+import numpy as np
+
+def imshow(img):
+    img = img / 2 + 0.5
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+# show sample image
+# dataiter = iter(trainloader)
+# images, labels = dataiter.next()
+# imshow(torchvision.utils.make_grid(images))
+# print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+# define CNN
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+        # TO-DO: 컨볼루션 모델을 더 딮하게 구성해보기
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        return x
+
+net = Net()
+print("--------- describe model ------------")
+print(net)
+
+# Define Optimimzer
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.0001, momentum=0.9)
+
+print("----------------------------------")
+print("--------- train model ------------")
+
+# Train network
+for epoch in range(10):
+    running_loss = 0.0
+
+    for i, data in enumerate(trainloader, 0):
+        inputs, labels = data
+
+        optimizer.zero_grad()
+
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss = loss.item()
+
+        if i % 100 == 99:
+            print("[epoch: %d, step: %d] loss : %.5f" % (epoch + 1, i + 1, running_loss))
+
+print("Finished")
+
+# Test Network
+total = 0
+correct = 0
+with torch.no_grad():
+    for i, data in enumerate(testloader, 0):
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (100 * correct / total))
+
+# TO-DO: 각 클래스별 정확도 계산하기
